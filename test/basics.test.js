ea78f424e931dfea3acc78bc4724096092b5d59a
Bruce MacNaughton <bmacnaughton@gmail.com>
2021-03-21 09:55:12 -0700

    codeword => code-word (#3)
    * codeword => code-word

--- test/basics.test.js
+++ test/basics.test.js
@@ -49,15 +49,7 @@ const codeWordTests = {
 };
 
 describe('basic tests', function () {
-  /**
-  const alpha = decodeRanges('A-Za-z');
-const numeric = decodeRanges('0-9');
-const alphanumeric = decodeRanges('A-Za-z0-9');
-const hex = decodeRanges('a-f0-9');
-const HEX = decodeRanges('A-F0-9');
-const base58 = decodeRanges('A-HJ-NP-Za-km-z1-9');
-   */
-  it('generates codeword strings correctly', function() {
+  it('generates code-word strings correctly', function() {
     const codeWords = Object.keys(codeWordTests);
     for (let i = 0; i < maxTests; i++) {
       for (let j = 0; j < codeWords.length; j++) {
